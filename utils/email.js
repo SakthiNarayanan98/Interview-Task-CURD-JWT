ce6a894bcb24f7f121e88a7e8ac61042495d0733
sakthinarayanan <sakthinarayanan@twilightsoftwares.com>
2024-12-04 13:08:19 +0530

    initial commit

--- utils/email.js
+++ utils/email.js
@@ -10,10 +10,10 @@ const sendEmail = async (to, subject, text) => {
   });
 
   const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    text,
+    from: process.env.EMAIL_USER, 
+    to: 'sakthinarayanan.rk@gmail.com',
+    subject: 'Interview Task',  
+    text: ' I have completed the task as assigned.Kindly review it and let me know if you have any feedback. ',
   };
 
   try {
